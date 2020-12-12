167aa64d31a156ec58870eec08e7cbb590676e19
Marina Oliveira <marina09oliveira@gmail.com>
2020-12-12 20:33:10 -0300

    Local variables & template variables

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ import useRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
 import routes from './routes';
+import { localsMiddleware } from './midllewares';
 
 const app = express();
 
 //middlewares
+app.use(helmet()); // segurança
+app.set('view engine', 'pug');
 app.use(bodyParser.json()); //está ligado com autenticação
 app.use(bodyParser.urlencoded({ extended: true })); //o que o usuário esta enviando
 app.use(cookieParser());
 app.use(morgan('dev')); // log das coisas do browser e da aplicação
-app.use(helmet()); // segurança
+
+// create global variables for the app
+
+app.use(localsMiddleware);
 
 // para as rotas usamos use
-app.set('view engine', 'pug');
 app.use(routes.home, globalRouter); // global route = home, login etc
 app.use(routes.users, useRouter);
 app.use(routes.videos, videoRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,3 @@
-export const join = (req, res) => res.send('Join');
-export const login = (req, res) => res.send('Login');
-export const logout = (req, res) => res.send('Logout');
+export const join = (req, res) => res.send('join');
+export const login = (req, res) => res.send('login');
+export const logout = (req, res) => res.send('logout');
