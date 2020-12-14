76c12e18e4e866896be35c245cda6870cae6cdc2
Marina Oliveira <marina09oliveira@gmail.com>
2020-12-14 10:03:15 -0300

    Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,23 +1,23 @@
-export const home = (req, res) => res.render('home', { pageTitle: 'Home' }); // está renderizando a home.pug
+import { videos } from '../db';
+
+export const home = (req, res) =>
+  res.render('home', { pageTitle: 'Home', videos }); // está renderizando a home.pug
 
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  console.group(searchingBy);
   res.render('search', { pageTitle: 'Search', searchingBy });
 };
 
-export const videos = (req, res) =>
-  res.render('videos', { pageTitle: 'Videos' });
-
-export const upload = (req, res) => res.render('Upload');
+export const upload = (req, res) =>
+  res.render('upload', { pageTitle: 'Upload' });
 
 export const videoDetail = (req, res) =>
-  res.render('Video Detail', { pageTitle: 'Video Detail' });
+  res.render('videoDetail', { pageTitle: 'Video Detail' });
 
 export const editVideo = (req, res) =>
-  res.render('Edit Video', { pageTitle: 'Edit Video' });
+  res.render('editVideo', { pageTitle: 'Edit Video' });
 
 export const deleteVideo = (req, res) =>
-  res.render('Delete Video', { pageTitle: 'Delete Video' });
+  res.render('deleteVideo', { pageTitle: 'Delete Video' });
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 32348,
+    title: 'Video awesome',
+    description: 'video qualquer',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 90909,
+      name: 'Marina',
+      email: 'marina@gmail.com',
+    },
+  },
+  {
+    id: 32349,
+    title: 'Video super 2',
+    description: 'video qualquer 2',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 90909,
+      name: 'Marina',
+      email: 'marina@gmail.com',
+    },
+  },
+  {
+    id: 32350,
+    title: 'Video topster',
+    description: 'video qualquer',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 90909,
+      name: 'Marina',
+      email: 'marina@gmail.com',
+    },
+  },
+  {
+    id: 32351,
+    title: 'Video uis',
+    description: 'video qualquer',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 90909,
+      name: 'Marina',
+      email: 'marina@gmail.com',
+    },
+  },
+];
