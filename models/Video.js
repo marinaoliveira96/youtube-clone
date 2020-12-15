dfe6456a3ecbf48c123ea2c06977b1ac47eeac8e
Marina Oliveira <marina09oliveira@gmail.com>
2020-12-15 17:56:50 -0300

    comment model

--- models/Video.js
+++ models/Video.js
@@ -19,6 +19,12 @@ const VideoSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  coments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
 });
 
 //document shape
